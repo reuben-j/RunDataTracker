abf2fc46601833b0a7439e4741ecd43fc0e5c10a
reuben-j <reubenjarvela@gmail.com>
2024-08-20 12:29:37 +0100

    Initial Commit

---
+++ myCode.cpp
@@ -0,0 +1,19 @@
+//
+//  myCode.cpp
+//  example_osx_opengl2
+//
+//  Created by Reuben Jarvela on 20/08/2024.
+//  Copyright © 2024 ImGui. All rights reserved.
+//
+
+#include "myCode.hpp"
+#include "imgui.h"
+
+
+namespace myCode {
+
+void renderMainWindow() {
+    ImGui::Begin("Run Tracker");
+    ImGui::End();
+}
+}
---
+++ myCode.hpp
@@ -0,0 +1,18 @@
+//
+//  myCode.hpp
+//  example_osx_opengl2
+//
+//  Created by Reuben Jarvela on 20/08/2024.
+//  Copyright © 2024 ImGui. All rights reserved.
+//
+
+#ifndef myCode_hpp
+#define myCode_hpp
+
+#include <stdio.h>
+
+#endif /* myCode_hpp */
+
+namespace myCode {
+    void renderMainWindow();
+}
